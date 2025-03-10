6e5a41b57bf9988fac01152dbc3cb06efc111eea
Seba <sebastian.3h@gmail.com>
2025-03-10 19:51:46 -0300

    feat: clase 01-basic-types

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,13 @@
+
+//let name = 'Strider';
+const name = 'Strider';
+let hpPoints : number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({
+    name, hpPoints, isAlive
+});
+
+export {};
